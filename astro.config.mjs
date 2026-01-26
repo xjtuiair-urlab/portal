2fda7ce8aab1acaff30de6220dd21e4d77624ddb
Qinghe <mikanaffine@outlook.com>
2026-01-26 16:50:04 +0800

    feat: migrate github

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mikanaffine.github.io',
-    base: '/cag-portal',
+    site: 'https://xjtuiair-urlab.github.io',
+    base: '/portal',
 });
